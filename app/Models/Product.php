1f77ca6e17b290370f10e0c529687de8c5ff614c
EdgarEldy <ed.eldy21@live.com>
2026-09-03 12:55:09 +0400

    feat(orders): add Customer::orders() and Product::orders()

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,7 @@ use Database\Factories\ProductFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -36,5 +37,10 @@ class Product extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
     }
 }
